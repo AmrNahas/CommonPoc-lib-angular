74cc9f2cfea88feb19e6f714973fa14126be71c1
amrelnahas4 <amrelnahas4@gmail.com>
2021-02-18 18:48:36 +0300

    skeleton

--- src/app/pages/publicPages/welcome/welcome.component.ts
+++ src/app/pages/publicPages/welcome/welcome.component.ts
@@ -46,39 +46,9 @@ export class welcomeComponent extends UtilityController implements OnInit {
     this.formsManager.upsert('fSearch', this.form);
   }
 
-    loadSvTypes()  {
-    this.dropDownServices.getAllSvTypesList().subscribe(data=>{
-      this.types=data;
-    })
-  }
+  ngOnInit(): void {
 
-    loadCityList()  {
-      this.dropDownServices.getAllCitiesForCountryDropDown(1).subscribe(data=>{
-      this.citiesDropDown=data;
-    })
-  }
-
-  ngOnInit() {
-    this.loadCityList();
-    this.loadSvTypes();
-    this.cities = [
-            { image: 'assets/img/landing/jeddah.jpg', nameAr: 'جدة',nameEn: 'Jeddah',subtitleAr: 'سحر البحر الاحمر ',subtitleEn: 'The magic of the Red Sea', desc: "Some quick example text to build on the card title and make up the bulk of the card's content.", followers: 10 },
-            { image: 'assets/img/landing/dammam.jpg' ,nameAr: 'الدمام',nameEn: 'Dammam',subtitleAr: 'ساحل بحرالخليج العربى ',subtitleEn: 'The coast of the Arabian Gulf', desc: "Some quick example text to build on the card title and make up the bulk of the card's content.", followers: 28 },
-
-    ]
-
-    this.activities = [
-      { image: 'assets/img/landing/boats/b1.jpg', nameAr: 'مركبات بحرية ',nameEn: 'Boats',subtitleAr: 'مراكب , قوارب و الموتوسكلات الشاطئية ',subtitleEn: 'Boats', desc: "Some quick example text to build on the card title and make up the bulk of the card's content.", followers: 10 },
-      { image: 'assets/img/landing/snorkel.jpg' ,nameAr: 'رحلات بحرية',nameEn: 'Cruises ',subtitleAr: 'الرحلات بحرية و الغطس',subtitleEn: 'Cruises and Snorkeling', desc: "Some quick example text to build on the card title and make up the bulk of the card's content.", followers: 28 },
-      { image: 'assets/img/landing/diving.jpg' ,nameAr: 'دورات بحرية',nameEn: 'Marine Courses',subtitleAr: 'الدورات بحرية ودورات الغوص',subtitleEn: 'Marine Courses', desc: "Some quick example text to build on the card title and make up the bulk of the card's content.", followers: 28 },
-
-    ]
-  }
-  search() {
-    this.dest=this.form.controls['dest'].value;
-    this.num=this.form.controls['num'].value;
-    this.type=this.form.controls['type'].value;
-     this.router.navigate(['/search'], { queryParams: { dest: this.dest ,num: this.num ,type: this.type  } });
+    this.isLoggedIn = this.authService.validateToken();
   }
 
 
